08aa4e248a95ed599c1943669149308df0d7f0a6
schollsebastian <scholl.sebastian@gmx.at>
2022-01-18 15:03:16 +0100

    Use the new discord.js API

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,39 @@
-import Discord from 'discord.js';
+import { Client, Intents } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}];
 const token = process.env['NODE_ENV'] === 'development' ?
-  process.env['DISCORD_DEV_TOKEN'] :
-  process.env['DISCORD_PROD_TOKEN'];
-const client = new Discord.Client();
+  process.env['DISCORD_DEV_TOKEN']! :
+  process.env['DISCORD_PROD_TOKEN']!;
+const rest = new REST({ version: '9' }).setToken(token);
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user!.tag}`);
+client.on('ready', client => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  client.guilds.cache.forEach(guild => {
+    rest.put(Routes.applicationGuildCommands(client.user.id, guild.id), { body: commands })
+      .catch(console.error);
+  });
 });
 
-client.on('message', message => {
-  console.log(message.content);
+client.on('interactionCreate', async interaction => {
+  if (interaction.isCommand()) {
+    switch (interaction.commandName) {
+      case 'ping':
+        interaction.reply('Pong!')
+          .catch(console.error);
+        break;
+    }
+  }
 });
 
-client.login(token).catch(console.error);
+client.login(token)
+  .catch(console.error);
